2f4b23445d2c9976511b1b387e249a7c921d821a
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-06-10 23:57:36 +0200

    models: project & video

--- DevelopigCommunityService/Models/Somaya/Project.cs
+++ DevelopigCommunityService/Models/Somaya/Project.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevelopigCommunityService.Models.Bassal;
+using DevelopigCommunityService.Models.Ebtisam;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,12 @@ namespace DevelopigCommunityService.Models.Somaya
 {
     public class Project
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool CodeView { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+            = new HashSet<Student>();
+        public virtual ICollection<Individual> Individuals { get; set; }
+            = new HashSet<Individual>();
     }
 }
--- DevelopigCommunityService/Models/Somaya/ProjectVideos.cs
+++ DevelopigCommunityService/Models/Somaya/ProjectVideos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace DevelopigCommunityService.Models.Somaya
 {
     public class ProjectVideos
     {
+        public int Id { get; set; }
+        public string VideoURL { get; set; }
+        [ForeignKey("Project")]
+        public int ProjectId { get; set; }
     }
 }
