47cf3d8d21ac4d42972f3d3005e01e3af14e79e7
Ahmed Elbassal <ahmed.bassal.italy@gmail.com>
2021-06-12 01:54:43 +0200

    update enetity

--- DevelopigCommunityService/Models/Reham/ProjectPhotos.cs
+++ DevelopigCommunityService/Models/Reham/ProjectPhotos.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DevelopigCommunityService.Models.Somaya;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +11,10 @@ namespace DevelopigCommunityService.Models.Reham
     {
         public int Id { get; set; }
         public String Url { get; set; }
+
+        [ForeignKey("Project")]
+        public int ProjectId { get; set; }
+        public virtual Project Project { get; set; }
 
         // some other properties
 
