9767ecca94642ac3475efaa8686589e6a7de045a
ebtesamahmed <ebtesamahmed391998@gmail.com>
2021-06-11 18:01:01 +0200

    studentProject class

---
+++ DevelopigCommunityService/Models/Ebtisam/StudentProject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using DevelopigCommunityService.Models.Somaya;
+
+namespace DevelopigCommunityService.Models.Ebtisam
+{
+    public class StudentProject
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int ProjectId { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int StudentId { get; set; }
+        public Project project { get; set; }
+        public Student student { get; set; }
+    }
+}
